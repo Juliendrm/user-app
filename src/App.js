05175e9ac43cab7bbd0be751ee359a50467687b4
Julien <benedetjulien@gmail.com>
2022-12-10 00:32:18 +0100

    user component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import React from "react";
+import AddUser from "./components/AddUsers";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
