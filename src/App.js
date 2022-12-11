b05287f3fb3e82dcda9d9e014ee7732b2312245b
Julien <benedetjulien@gmail.com>
2022-12-11 22:53:31 +0100

    component for list of users added +  map logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddUser from "./components/Users/AddUsers";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
